ae599d7ab92a070db3090ab89237b5bc9010bfa5
Giorgi Bliadze <bliadze1997@gmail.com>
2024-06-16 16:54:08 +0400

    marker1 test

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -13,7 +13,7 @@ const MapComponent = () => {
   const onLoad = useCallback((map) => {
     setMap(map);
     const icon = {
-      url: "/custom-marker.svg",
+      url: "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='%23ff6347' d='M12 2C8.14 2 5 5.14 5 9c0 4.67 6.31 11.55 6.59 11.84.25.26.66.26.91 0 .29-.29 6.59-7.17 6.59-11.84 0-3.86-3.14-7-7-7zm0 12c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z'/%3E%3C/svg%3E",
       scaledSize: new window.google.maps.Size(40, 40),
     };
     setMarkerIcon(icon);
@@ -23,7 +23,7 @@ const MapComponent = () => {
     <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
       <div className="flex justify-center items-center w-full h-screen">
         <GoogleMap
-          mapContainerClassName="w-full h-3/5 max-w-5xl rounded-lg shadow-lg"
+          mapContainerClassName="w-full h-3/5 max-w-5xl rounded shadow"
           center={center}
           zoom={15}
           onLoad={onLoad}
