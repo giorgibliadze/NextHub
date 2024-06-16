7773f07a2047b0c385bb8f6bba74a95325ae279f
Giorgi Bliadze <bliadze1997@gmail.com>
2024-06-16 16:59:06 +0400

    marker in purple and red

--- components/MapComponent.js
+++ components/MapComponent.js
@@ -13,7 +13,7 @@ const MapComponent = () => {
   const onLoad = useCallback((map) => {
     setMap(map);
     const icon = {
-      url: "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='%23ff6347' d='M12 2C8.14 2 5 5.14 5 9c0 4.67 6.31 11.55 6.59 11.84.25.26.66.26.91 0 .29-.29 6.59-7.17 6.59-11.84 0-3.86-3.14-7-7-7zm0 12c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z'/%3E%3C/svg%3E",
+      url: "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='%23B83280' d='M12 2C8.14 2 5 5.14 5 9c0 4.67 6.31 11.55 6.59 11.84.25.26.66.26.91 0 .29-.29 6.59-7.17 6.59-11.84 0-3.86-3.14-7-7-7zm0 12c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z'/%3E%3C/svg%3E",
       scaledSize: new window.google.maps.Size(40, 40),
     };
     setMarkerIcon(icon);
