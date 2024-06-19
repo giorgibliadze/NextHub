86b12970956ace95f495f5232efd2997a436a923
Giorgi Bliadze <bliadze1997@gmail.com>
2024-06-19 09:22:51 +0400

    about icon slider

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,9 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.min.css";
+import SwiperCore, { Autoplay } from "swiper";
 
 // icons
 import {
@@ -28,10 +32,8 @@ import Circles from "../../components/Circles";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
 
-// react-slick
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+// Install Swiper modules
+SwiperCore.use([Autoplay]);
 
 const icons = [
   { component: FaHtml5, name: "HTML5", color: "#E34F26" },
@@ -55,42 +57,6 @@ const icons = [
 ];
 
 const About = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
-
   return (
     <div className="min-h-screen py-16 flex items-center bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
       <Circles />
@@ -105,7 +71,7 @@ const About = () => {
           ჩვენს შესახებ
         </motion.h1>
         <motion.p
-          variants={fadeIn("up", 0.2)}
+          variants={fadeIn("up", 0.1)}
           initial="hidden"
           animate="show"
           exit="hidden"
@@ -115,29 +81,49 @@ const About = () => {
           ვართ SEO ოპტიმიზაციაში, ვებსაიტების შექმნაში, სოციალური მედიის
           მენეჯმენტსა და ანალიტიკაში.
         </motion.p>
-        <Slider {...settings}>
+        <Swiper
+          spaceBetween={20}
+          slidesPerView={7}
+          autoplay={{
+            delay: 1000,
+            disableOnInteraction: false,
+          }}
+          breakpoints={{
+            1024: {
+              slidesPerView: 7,
+              spaceBetween: 30,
+            },
+            600: {
+              slidesPerView: 4,
+              spaceBetween: 20,
+            },
+            480: {
+              slidesPerView: 3,
+              spaceBetween: 10,
+            },
+          }}
+          className="mySwiper"
+        >
           {icons.map((icon, index) => {
             const IconComponent = icon.component;
             return (
-              <motion.div
-                key={index}
-                variants={fadeIn("up", 0.3 + index * 0.1)}
-                initial="hidden"
-                animate="show"
-                exit="hidden"
-                className="p-6 m-4 rounded-lg shadow-lg flex items-center bg-gradient-to-r from-purple-900 via-purple-700 to-purple-400 transform transition-transform hover:scale-105 hover:shadow-2xl"
-              >
-                <IconComponent
-                  className="text-6xl transition-colors duration-300"
-                  style={{ color: icon.color }}
-                />
-                <h2 className="text-xl font-bold ml-4 text-white">
-                  {icon.name}
-                </h2>
-              </motion.div>
+              <SwiperSlide key={index}>
+                <motion.div
+                  variants={fadeIn("up", 0.1)}
+                  initial="hidden"
+                  animate="show"
+                  exit="hidden"
+                  className="p-6 m-4 rounded-lg shadow-lg flex items-center bg-gradient-to-r from-purple-900 via-purple-700 to-purple-400 transform transition-transform hover:scale-105 hover:shadow-2xl"
+                >
+                  <IconComponent
+                    className="text-6xl transition-colors duration-300"
+                    style={{ color: icon.color }}
+                  />
+                </motion.div>
+              </SwiperSlide>
             );
           })}
-        </Slider>
+        </Swiper>
       </div>
     </div>
   );
