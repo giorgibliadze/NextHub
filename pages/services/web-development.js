654d3b0841fab0afad85a5d0deba3f0060f7a084
Giorgi Bliadze <bliadze1997@gmail.com>
2024-06-20 01:05:03 +0400

    web development first part

--- pages/services/web-development.js
+++ pages/services/web-development.js
@@ -1,107 +1,111 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants"; // Importing fadeIn animation from your variants file
-import Link from "next/link";
-//counter
-import CountUp from "react-countup";
+import CountUp from "react-countup"; // Importing CountUp for the counter animation
 
 const WebDevelopment = () => {
   // Define an array of pricing packages
   const packages = [
     {
       name: "Startup",
-      price: "1000",
+      startPrice: 1000,
+      price: 2500,
       features: [
-        "5 გვერდი",
-        "ენების არჩევა",
-        "სამართავი პანელი",
+        "1 გვერდი",
+        "მობილური ვერსია (Responsive)",
         "სტატისტიკაზე წვდომა",
         "სოციალური ქსელების ინტეგრაცია",
         "Slideshow - სურათების სლაიდშოუ",
+        "ჩვენს შესახებ",
+        "სიახლეები",
+        "სერვისები",
+        "პარტნიორების ლოგოები",
+        "ფოტო გალერეა",
         "ონლაინ ჩატი",
+        "კონტაქტი",
         "1 თვე მხარდაჭერის სერვისი",
       ],
     },
     {
       name: "Standard",
-      price: "2500",
+      startPrice: 2500,
+      price: 4000,
       features: [
-        "10 გვერდი",
-        "მოიცავს startup პაკეტს",
-        "E-commerce ფუნქციონალი",
-        "ონლაინ გადახდები",
-        "ავტორიზაციის ფუნქცია",
-        "ქეშირების მოდულის კონფიგურაცია",
+        "5 გვერდი",
+        "მარტივი სამართავი პანელი",
+        "მობილური ვერსია (Responsive)",
+        "სტატისტიკაზე წვდომა",
+        "მთავარი",
+        "ჩვენს შესახებ",
+        "სიახლეები",
+        "სერვისები",
+        "კონტაქტი",
+        "ფილიალები",
+        "პროექტები – მიმდინარე, დასრულებული",
+        "ფოტო გალერეა",
+        "ვიდეო გალერეა",
+        "ვიზიტორების სტატისტიკა",
+        "სოციალური ქსელების ინტეგრაცია",
+        "Slideshow – სურათების სლაიდშოუ",
+        "ონლაინ ჩატი",
+        "პოპულარული სიახლეები",
+        "კლიენტების მოდული",
+        "პარტნიორების ლოგოები",
         "3 თვე მხარდაჭერის სერვისი",
       ],
     },
     {
       name: "Premium",
-      price: "5000",
+      startPrice: 5000,
+      price: 7000,
       features: [
-        "მოიცავს standard პაკეტს",
-        "SEO მოდულის კონფიგურაცია",
-        "რთული პლაგინების კონფიგურაცია",
+        "მობილური ვერსია (Responsive)",
+        "სტატისტიკაზე წვდომა",
+        "სამართავი პანელი",
+        "პოპულარული სიახლეები",
+        "Top პროდუქცია",
+        "პარტნიორების ლოგოები",
+        "ონლაინ ჩატი",
+        "პროდუქციის ძიება",
+        "პროდუქციის სორტირება და ფილტრაცია",
+        "სასურველი პროდუქცია – Wish List",
+        "პროდუქციის კალათაში დამატება",
+        "პროდუქციის შეკვეთა",
+        "ვიზიტორების სტატისტიკა",
+        "სოციალური ქსელების ინტეგრაცია",
+        "რეგისტრაციის ფორმა – ფიზ, იურიდ.",
+        "Facebook-ით რეგისტრაცია",
+        "Google-თ რეგისტრაცია",
+        "პროდუქციის შეძენა Visa/Mastercard/Amex ბარათებით",
+        "პროდუქციის შეძენა ონლაინ განვადებით",
+        " გაყიდვების სტატისტიკა",
+        " ადგილზე მიტანის მოდული",
         "B2B & B2C მოდულები",
         "6 თვე მხარდაჭერის სერვისი",
+        "და სხვა",
       ],
     },
   ];
 
-  const nextJsPackages = [
-    {
-      name: "Basic Package",
-      price: "1500",
-      features: [
-        "Basic setup",
-        "5 გვერდი",
-        "რესპონსიული დიზაინი",
-        "SEO setup",
-        "1 თვე მხარდაჭერის სერვისი",
-      ],
-    },
-    {
-      name: "Standard Package",
-      price: "3000",
-      features: [
-        "Standard setup",
-        "10 გვერდი",
-        "გაფართოებული SEO დაყენება",
-        "მორგებული დიზაინი",
-        "ანალიტიკა",
-        "API ინტეგრაცია",
-        "3 თვე მხარდაჭერის სერვისი",
-      ],
-    },
-    {
-      name: "Premium Package",
-      price: "6000",
-      features: [
-        "ულიმიტო გვერდები",
-        "გაფართოებული SEO დაყენება",
-        "ინდივიდუალური დიზაინი ანიმაციებით",
-        "სისწრაფის ოპტიმიზაცია",
-        "ანალიტიკა",
-        "E-commerce ფუნქციონალი",
-        "ფაილები",
-        "6 თვე მხარდაჭერის სერვისი",
-      ],
-    },
-  ];
+  const [expandedPackage, setExpandedPackage] = useState(null);
+
+  const handleShowMore = (index) => {
+    setExpandedPackage(expandedPackage === index ? null : index);
+  };
 
   return (
-    <div className="min-h-screen bg-primary/30 py-12">
+    <div className="min-h-screen py-12">
       <div className="container mx-auto text-center">
         <motion.h1
           variants={fadeIn("up", 0.2)}
           initial="hidden"
           animate="show"
           exit="hidden"
-          className="text-4xl font-bold mt-20 mb-12"
+          className="text-4xl font-bold mt-20 mb-12 text-gray-800"
         >
-          Wordpress
+          Web Development
         </motion.h1>
 
-        {/* Pricing Segments for WordPress */}
         <motion.div
           variants={fadeIn("up", 0.2)}
           initial="hidden"
@@ -112,61 +116,44 @@ const WebDevelopment = () => {
           {packages.map((pkg, index) => (
             <div
               key={index}
-              className="p-6 rounded-lg shadow-lg max-w-[300px] text-left"
+              className={`p-6 rounded-lg shadow-lg bg-white transform transition-transform duration-300 ${
+                expandedPackage === index ? "max-h-[1000px]" : "max-h-[400px]"
+              }`}
             >
-              <h2 className="text-2xl font-bold mb-4">{pkg.name}</h2>
-              <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                ₾ <CountUp start={500} end={pkg.price} duration={1} /> +
+              <h2 className="text-2xl font-bold mb-4 text-gray-800">
+                {pkg.name}
+              </h2>
+              <div className="text-xl xl:text-3xl font-extrabold text-accent mb-2">
+                <span className="text-gray-800">₾</span>{" "}
+                <CountUp
+                  start={pkg.startPrice}
+                  end={pkg.startPrice}
+                  duration={0}
+                />
+                <span className="text-gray-800"> - </span>
+                <CountUp start={pkg.startPrice} end={pkg.price} duration={1} />
               </div>
-              <ul className="list-disc list-inside mb-4">
-                {pkg.features.map((feature, index) => (
-                  <li key={index}>{feature}</li>
-                ))}
+              <ul className="list-disc list-inside mb-4 text-left text-gray-700">
+                {pkg.features
+                  .slice(
+                    0,
+                    expandedPackage === index ? pkg.features.length : 10
+                  )
+                  .map((feature, featureIndex) => (
+                    <li key={featureIndex}>{feature}</li>
+                  ))}
               </ul>
-              {/* Add a button or link for purchasing */}
+              <div className="flex justify-center">
+                <button
+                  className="w-[150px] lg:w-[200px] py-3 rounded-full bg-gradient-to-r from-red-500 to-orange-500 text-white hover:from-red-600 hover:to-orange-600 transition-colors duration-300"
+                  onClick={() => handleShowMore(index)}
+                >
+                  {expandedPackage === index ? "Show Less" : "Learn More"}
+                </button>
+              </div>
             </div>
           ))}
         </motion.div>
-      </div>
-      <div className="min-h-screen bg-primary/30 py-12">
-        <div className="container mx-auto text-center">
-          <motion.h1
-            variants={fadeIn("up", 0.2)}
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            className="text-4xl font-bold mt-20 mb-12"
-          >
-            Next
-          </motion.h1>
-
-          {/* Pricing Segments for Next.js Apps */}
-          <motion.div
-            variants={fadeIn("up", 0.2)}
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            className="mt-12 grid grid-cols-1 lg:grid-cols-3 gap-8 justify-center"
-          >
-            {nextJsPackages.map((pkg, index) => (
-              <div
-                key={index}
-                className="p-6 rounded-lg shadow-lg max-w-[300px] text-left"
-              >
-                <h2 className="text-2xl font-bold mb-4">{pkg.name}</h2>
-                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
-                  ₾ <CountUp start={0} end={pkg.price} duration={1} /> +
-                </div>
-                <ul className="list-disc list-inside mb-4">
-                  {pkg.features.map((feature, index) => (
-                    <li key={index}>{feature}</li>
-                  ))}
-                </ul>
-                {/* Add a button or link for purchasing */}
-              </div>
-            ))}
-          </motion.div>
-        </div>
       </div>
     </div>
   );
