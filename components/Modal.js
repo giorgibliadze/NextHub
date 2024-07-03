b225029923b0955d904d6216015dadcfc63bd3aa
giorgibliadze <bliadze1997@gmail.com>
2024-07-04 02:23:30 +0400

    text color change in modal

--- components/Modal.js
+++ components/Modal.js
@@ -43,13 +43,13 @@ const Modal = ({ isOpen, onClose, cardData }) => {
       });
       const data = await res.json();
       if (res.ok) {
-        setSuccessMessage("Email sent successfully");
+        setSuccessMessage("შეკვეთა მიღებულია");
         formRef.current.reset(); // Reset form fields
       } else {
-        setError(`Failed to send email: ${data.message}`);
+        setError(`შეკვეთის მიღება ვერ მოხერხდა: ${data.message}`);
       }
     } catch (err) {
-      setError(`Failed to send email: ${err.message}`);
+      setError(`შეკვეთის მიღება ვერ მოხერხდა: ${err.message}`);
     } finally {
       setLoading(false);
     }
@@ -63,7 +63,7 @@ const Modal = ({ isOpen, onClose, cardData }) => {
         initial={{ opacity: 0, scale: 0.9 }}
         animate={{ opacity: 1, scale: 1 }}
         exit={{ opacity: 0, scale: 0.9 }}
-        className="flex flex-col items-center p-6 border-4 border-glow bg-black rounded-lg shadow-lg transition-shadow duration-300 ease-in-out hover:shadow-2xl animate-container-glow w-11/12 md:w-1/2 lg:w-1/3"
+        className="flex flex-col lg:mt-[280px] smx:mt-[300px] items-center p-6 border-4 border-glow bg-black rounded-lg shadow-lg transition-shadow duration-300 ease-in-out hover:shadow-2xl animate-container-glow w-11/12 md:w-1/2 lg:w-1/3"
       >
         <h3 className="text-2xl font-semibold text-gray-100 mb-4">
           {cardData.title}
@@ -84,7 +84,7 @@ const Modal = ({ isOpen, onClose, cardData }) => {
           initial="hidden"
           animate="show"
           exit="hidden"
-          className="flex-1 flex flex-col gap-6 w-full mx-auto border-glow animate-container-glow"
+          className="flex-1 flex flex-col gap-6 w-full mx-auto border-glow animate-container-glow text-white"
           onSubmit={send}
         >
           <div className="flex flex-col md:flex-row gap-6 w-full">
@@ -93,7 +93,7 @@ const Modal = ({ isOpen, onClose, cardData }) => {
               id="name"
               name="name"
               placeholder="სახელი"
-              className="input text-center w-full text-white"
+              className="text-white bg-transparent border-b border-gray-500 focus:outline-none focus:border-accent input text-center w-full focus:text-accent"
               value={name}
               onChange={(e) => setName(e.target.value)}
               required
@@ -103,7 +103,7 @@ const Modal = ({ isOpen, onClose, cardData }) => {
               id="email"
               name="email"
               placeholder="მაილი"
-              className="input text-center w-full text-white"
+              className="text-white bg-transparent border-b border-gray-500 focus:outline-none focus:border-accent input text-center w-full focus:text-accent"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
@@ -114,7 +114,7 @@ const Modal = ({ isOpen, onClose, cardData }) => {
             id="phone"
             name="phone"
             placeholder="ტელეფონი"
-            className="input text-center w-full text-white"
+            className="text-white bg-transparent border-b border-gray-500 focus:outline-none focus:border-accent input text-center w-full focus:text-accent"
             value={phone}
             onChange={(e) => setPhone(e.target.value)}
             required
@@ -124,7 +124,7 @@ const Modal = ({ isOpen, onClose, cardData }) => {
             id="subject"
             name="subject"
             placeholder="თემა"
-            className="input text-center w-full text-white"
+            className="text-white bg-transparent border-b border-gray-500 focus:outline-none focus:border-accent input text-center w-full focus:text-accent"
             value={subject}
             onChange={(e) => setSubject(e.target.value)}
             required
@@ -133,21 +133,23 @@ const Modal = ({ isOpen, onClose, cardData }) => {
             id="message"
             name="message"
             placeholder="შეტყობინება"
-            className="textarea text-center w-full text-white"
+            className="text-white bg-transparent border-b border-gray-500 focus:outline-none focus:border-accent textarea text-center w-full focus:text-accent"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
             required
           ></textarea>
-          <button
-            type="submit"
-            className="btn rounded-full border border-white max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group"
-            disabled={loading}
-          >
-            <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
-              {loading ? "შეკვეთა..." : "შეკვეთა"}
-            </span>
-            <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
-          </button>
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="btn rounded-full border border-white max-w-[170px] px-8 transition-all duration-300 flex items-center justify-center overflow-hidden hover:border-accent group text-white"
+              disabled={loading}
+            >
+              <span className="group-hover:-translate-y-[120%] group-hover:opacity-0 transition-all duration-500">
+                {loading ? "შეკვეთა..." : "შეკვეთა"}
+              </span>
+              <BsArrowRight className="-translate-y-[120%] opacity-0 group-hover:flex group-hover:-translate-y-0 group-hover:opacity-100 transition-all duration-300 absolute text-[22px]" />
+            </button>
+          </div>
           {error && <p className="text-red-500 mt-2">{error}</p>}
           {successMessage && (
             <p className="text-green-500 mt-2">{successMessage}</p>
