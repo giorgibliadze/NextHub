f4482d9f9d6727b1d2c417e88a06f1b1bc08413b
giorgibliadze <bliadze1997@gmail.com>
2024-07-05 18:46:32 +0400

    map image change

---
+++ components/LazyLoad.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const LazyLoad = ({ children }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        setIsVisible(true);
+        observer.disconnect();
+      }
+    });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.disconnect();
+      }
+    };
+  }, []);
+
+  return <div ref={ref}>{isVisible ? children : null}</div>;
+};
+
+export default LazyLoad;
--- components/MapComponent.js
+++ components/MapComponent.js
@@ -1,5 +1,18 @@
 import React, { useState, useCallback } from "react";
-import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import dynamic from "next/dynamic";
+
+const LoadScript = dynamic(
+  () => import("@react-google-maps/api").then((mod) => mod.LoadScript),
+  { ssr: false }
+);
+const GoogleMap = dynamic(
+  () => import("@react-google-maps/api").then((mod) => mod.GoogleMap),
+  { ssr: false }
+);
+const Marker = dynamic(
+  () => import("@react-google-maps/api").then((mod) => mod.Marker),
+  { ssr: false }
+);
 
 const center = {
   lat: 41.729583,
@@ -7,13 +20,11 @@ const center = {
 };
 
 const MapComponent = () => {
-  const [map, setMap] = useState(null);
   const [markerIcon, setMarkerIcon] = useState(null);
 
   const onLoad = useCallback((map) => {
-    setMap(map);
     const icon = {
-      url: "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'%3E%3Cpath fill='%23B83280' d='M12 2C8.14 2 5 5.14 5 9c0 4.67 6.31 11.55 6.59 11.84.25.26.66.26.91 0 .29-.29 6.59-7.17 6.59-11.84 0-3.86-3.14-7-7-7zm0 12c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z'/%3E%3C/svg%3E",
+      url: "/public/location-map.webp",
       scaledSize: new window.google.maps.Size(40, 40),
     };
     setMarkerIcon(icon);
