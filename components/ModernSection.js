eb4ac5e6b2904557fa49becbd4ad2cac5b192a94
giorgibliadze <bliadze1997@gmail.com>
2024-07-06 15:52:00 +0400

    contacts modern margin top fix

--- components/ModernSection.js
+++ components/ModernSection.js
@@ -70,13 +70,13 @@ const ModernContainer = () => {
       initial="hidden"
       animate="show"
       exit="hidden"
-      className="w-full flex flex-col mt-[250px] md:flex-row items-center gap-8 z-40 px-4 md:px-16"
+      className="w-full flex flex-col lg:flex-row items-center gap-8 z-40 px-4 md:px-16 smx:mt-[290px] md:mt-[500px] lg:mt-[150px] lgx:mt-[250px]"
     >
       {contacts.map((contact, index) => (
         <div
           key={contact.id}
           className={classNames(
-            "container flex items-center p-4 w-full min-w-[250px] md:w-[450px] sm:w-[300px] h-[180px] bg-white rounded-lg shadow-md cursor-pointer mb-5 transition duration-300 transform hover:scale-105 group",
+            "container flex items-center p-4 w-full min-w-[250px] xl:w-[450px] xxl:w-[450px] xll:w-[450px] xl:h-[167px] lg:h-[147px] md:h-[150px] xxl:h-[450px] xll:h-[450px] sm:w-[300px] h-[180px] smx:w-[250px] smx:h-[100px]  bg-white rounded-lg shadow-md cursor-pointer mb-5 transition duration-300 transform hover:scale-105 group",
             {
               [contact.hoverBgColor]: hoveredIndex === index,
             }
