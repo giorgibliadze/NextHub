1e0f4b0a60a5f2267b3789d3618adf15659cd635
giorgibliadze <bliadze1997@gmail.com>
2024-07-07 23:58:04 +0400

    refactor: Update TopLeftImg component with optimized image loading and blending fix

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -2,12 +2,16 @@ import Image from "next/image";
 
 const TopLeftImg = () => {
   return (
-    <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50 will-change-transform will-change-opacity">
+    <div className="absolute left-0 top-0 z-10 w-[200px] xl:w-[400px] opacity-50 blend-fix">
       <Image
-        src="/top-left-img.png"
-        width={400}
-        height={400}
+        src="/top-left-img-removebg-preview.png"
+        width={411}
+        height={405}
         alt="Decorative Image"
+        priority // Ensure this image is always loaded first
+        placeholder="blur" // Adds a blur placeholder effect
+        blurDataURL="/path-to-small-placeholder-image.png" // Optional: a very small image for the blur effect
+        className="blend-fix-img" // New class to handle blending issues
       />
     </div>
   );
