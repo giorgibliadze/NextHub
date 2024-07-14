3e73a6eaeb8a8058c9c2986016704df2ad4d0792
giorgibliadze <bliadze1997@gmail.com>
2024-07-14 11:37:12 +0400

    ციფრულ რეალობად text add

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ import { fadeIn } from "../variants";
 const Home = () => {
   const words = ["იდეები", "სურვილები", "ოცნებები"];
   return (
-    <div className="bg-primary/60 h-full smx:h-[670px] sm:h-[600px] lg:h-[847px] xxl:h-[970px] xll:h-[865px]">
+    <div className="bg-primary/60 h-full smx:h-[650px] sm:h-[600px] lg:h-[847px] xxl:h-[970px] xll:h-[865px]">
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:text-left h-full container mx-auto ]">
@@ -37,6 +37,7 @@ const Home = () => {
             >
               <RotatingWords words={words} />
             </motion.span>
+            <br /> ციფრულ რეალობად
           </motion.h1>
           {/* subtitle */}
           <motion.p
