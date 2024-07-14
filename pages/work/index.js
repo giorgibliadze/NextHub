00a3df44b5d8b87ff8056e7d66e094225728e49a
giorgibliadze <bliadze1997@gmail.com>
2024-07-14 11:49:59 +0400

    refactor: Update container margin in work page

--- pages/work/index.js
+++ pages/work/index.js
@@ -12,7 +12,7 @@ const Work = () => {
   return (
     <div className="h-full bg-primary/30">
       <Circles />
-      <div className="container mx-auto lg:mt-[100px]">
+      <div className="container mx-auto lg:mt-[100px] mb-4">
         <div className="flex flex-col xl:flex-row gap-x-8">
           {/* text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
