923cf8f1234ebc1f0c92225568d2b2d73facbe2f
giorgibliadze <bliadze1997@gmail.com>
2024-07-15 13:32:03 +0400

    refactor: Update landing page background height for different screen sizes

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ import { fadeIn } from "../variants";
 const Home = () => {
   const words = ["იდეები", "სურვილები", "ოცნებები"];
   return (
-    <div className="bg-primary/60 h-full smx:h-[640px] sm:h-[600px] lg:h-[847px] xxl:h-[970px] xll:h-[865px]">
+    <div className="bg-primary/60 w-full h-full smx:h-[600px] sm:h-[600px] lg:h-[820px] xxl:h-[970px] xll:h-[865px]">
       {/* text */}
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:text-left h-full container mx-auto ]">
@@ -66,7 +66,7 @@ const Home = () => {
         </div>
 
         {/* image */}
-        <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        <div className="w-full h-full absolute right-0 bottom-0">
           {/* bg img */}
           <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
           {/* particles */}
