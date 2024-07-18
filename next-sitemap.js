264a718725eb89b83e6649642889df30917eb94d
giorgibliadze <bliadze1997@gmail.com>
2024-07-18 21:17:03 +0400

    chore: Update next-sitemap.config.js with additional sitemap entries

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,8 +1,16 @@
+// next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.next-hub.pro",
+  siteUrl: process.env.SITE_URL || "https://www.next-hub.pro",
   generateRobotsTxt: true,
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+  exclude: ["/api/*"],
   robotsTxtOptions: {
-    policies: [{ userAgent: "*", allow: "/" }],
+    additionalSitemaps: [
+      "https://www.next-hub.pro/sitemap.xml",
+      "https://www.next-hub.pro/sitemap-0.xml",
+    ],
   },
 };
