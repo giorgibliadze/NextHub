cbd2d3c56a73e569607f5ad13f076cd5ee592c4f
giorgibliadze <bliadze1997@gmail.com>
2024-07-20 19:56:50 +0400

    chore: Update sitemap URLs to reflect recent changes

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,59 +1,58 @@
-// next-sitemap.config.js
-/** @type {import('next-sitemap').IConfig} */
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 const getAllRoutes = () => {
   const staticPages = fs
-    .readdirSync(path.join(__dirname, 'pages'))
+    .readdirSync(path.join(__dirname, "pages"))
     .filter((file) => {
       return (
-        file.endsWith('.js') &&
-        !file.startsWith('_') &&
-        !file.includes('[[') &&
-        !file.includes('[')
+        file.endsWith(".js") &&
+        !file.startsWith("_") &&
+        !file.includes("[[") &&
+        !file.includes("[")
       );
     })
     .map((page) => {
-      const path = page.replace('.js', '');
-      return path === 'index' ? '/' : `/${path}`;
+      const path = page.replace(".js", "");
+      return path === "index" ? "/" : `/${path}`;
     });
 
-  // Add dynamic routes if necessary
   const dynamicRoutes = [
-    // Example dynamic routes, replace with your actual dynamic routes
-    '/services/analytics',
-    '/services/digital_marketing',
-    '/services/graphic_design',
-    '/services/seo',
-    '/services/soc_media',
-    '/services/tech_support',
-    '/services/web_developmemt',
-
-    // Add other dynamic routes here
+    "/services/analytics",
+    "/services/digital_marketing",
+    "/services/graphic_design",
+    "/services/seo",
+    "/services/soc_media",
+    "/services/tech_support",
+    "/services/web_development",
+    "/about",
+    "/contact",
+    "/services",
+    "/work",
   ];
 
   return [...staticPages, ...dynamicRoutes];
 };
 
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://www.next-hub.pro',
+  siteUrl: process.env.SITE_URL || "https://www.next-hub.pro",
   generateRobotsTxt: true,
-  changefreq: 'daily',
+  changefreq: "daily",
   priority: 0.7,
   sitemapSize: 5000,
-  exclude: ['/api/*'], // Excludes API routes only
+  exclude: ["/api/*"],
   additionalPaths: async (config) => {
     const allRoutes = getAllRoutes();
     return allRoutes.map((route) => ({
       loc: route,
-      changefreq: 'daily',
+      changefreq: "daily",
       priority: 0.7,
     }));
   },
   robotsTxtOptions: {
     additionalSitemaps: [
-      'https://www.next-hub.pro/sitemap-0.xml', // Ensure correct sitemap URL
+      "https://www.next-hub.pro/sitemap-0.xml",
+      "https://www.next-hub.pro/sitemap.xml",
     ],
   },
 };
