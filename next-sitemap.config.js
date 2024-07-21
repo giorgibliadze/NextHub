ec10c7bce425317fd2e4b67f5770cbfebf31e13a
giorgibliadze <bliadze1997@gmail.com>
2024-07-21 22:22:59 +0400

    chore: Disable generation of robots.txt in next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -38,7 +38,7 @@ const getAllRoutes = () => {
 
 module.exports = {
   siteUrl: process.env.SITE_URL || "https://www.next-hub.pro",
-  generateRobotsTxt: true,
+  generateRobotsTxt: false,
   changefreq: "daily",
   priority: 0.7,
   sitemapSize: 5000,
