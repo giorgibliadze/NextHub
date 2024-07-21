f22300c30c0dc997936f0f3cb48d41734c42c18c
giorgibliadze <bliadze1997@gmail.com>
2024-07-22 00:47:04 +0400

    chore: Add Google Tag Manager to home page for analytics tracking

--- pages/_app.js
+++ pages/_app.js
@@ -9,12 +9,13 @@ import { useRouter } from "next/router";
 
 //framer motion
 import { AnimatePresence, motion } from "framer-motion";
-
+import { GoogleTagManager } from "@next/third-parties/google";
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
 
   return (
     <Layout>
+      <GoogleTagManager gtmId="GTM-KHQ9N3M5" />
       <AnimatePresence mode="wait">
         <motion.div key={router.route}>
           {/* <Transition /> */}
