1107d94c788ee3eac24a7f18c8daab7297aec2b4
giorgibliadze <bliadze1997@gmail.com>
2024-07-22 18:48:45 +0400

    chore: Add NextSeo component to web_development page

--- pages/services/web_development.js
+++ pages/services/web_development.js
@@ -9,6 +9,7 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
 import Head from "next/head";
 import { GoogleTagManager } from "@next/third-parties/google";
+import { NextSeo } from "next-seo";
 const WebDevelopment = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedCardData, setSelectedCardData] = useState(null);
@@ -160,6 +161,60 @@ const WebDevelopment = () => {
 
   return (
     <>
+      <NextSeo
+        title="Next-Hub Solutions | Web Development"
+        description="მიაღწიეთ თქვენს მიზნებს, გამოიყენეთ სრული პოტენციალი და გაზარდეთ ბრენდის ცნობადობა და სანდოობა next-hub-ის გამოცდილ გუნდთან ერთად."
+        keywords="Next-Hub, ციფრული რეალობა, იდეები, სურვილები, ოცნებები, ბრენდის ცნობადობა, სანდოობა, ციფრული ტრანსფორმაცია, ვებ აპლიკაციები, სოციალური მედია, SEO, ანალიტიკა"
+        openGraph={{
+          type: "website",
+          locale: "ka_GE",
+          url: "https://www.next-hub.pro/services/web_development",
+          site_name: "Next-Hub Solutions",
+          title: "Next-Hub Solutions | Web Development",
+          description:
+            "მიაღწიეთ თქვენს მიზნებს, გამოიყენეთ სრული პოტენციალი და გაზარდეთ ბრენდის ცნობადობა და სანდოობა next-hub-ის გამოცდილ გუნდთან ერთად.",
+          images: [
+            {
+              url: "https://www.next-hub.pro/nexthub.jpg",
+              width: 800,
+              height: 600,
+              alt: "Next-Hub Solutions",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@NextHub",
+          site: "@NextHub",
+          cardType: "summary_large_image",
+          title: "Next-Hub Solutions | Web Development",
+          description:
+            "მიაღწიეთ თქვენს მიზნებს, გამოიყენეთ სრული პოტენციალი და გაზარდეთ ბრენდის ცნობადობა და სანდოობა next-hub-ის გამოცდილ გუნდთან ერთად.",
+          image: "https://www.next-hub.pro/nexthub.jpg",
+        }}
+        additionalMetaTags={[
+          {
+            name: "keywords",
+            content:
+              "Next-Hub, ციფრული რეალობა, იდეები, სურვილები, ოცნებები, ბრენდის ცნობადობა, სანდოობა, ციფრული ტრანსფორმაცია, ვებ აპლიკაციები, სოციალური მედია, SEO, ანალიტიკა",
+          },
+          {
+            name: "author",
+            content: "Next-Hub Solutions",
+          },
+          {
+            name: "viewport",
+            content: "width=device-width, initial-scale=1.0",
+          },
+          {
+            name: "robots",
+            content: "index, follow",
+          },
+          {
+            name: "changefreq",
+            content: "always",
+          },
+        ]}
+      />
       <GoogleTagManager gtmId="GTM-KHQ9N3M5" />
       {isModalOpen ? (
         <Modal
