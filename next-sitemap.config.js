fd24f975147e6bab1e8b256e372d2fb7010710e6
giorgibliadze <bliadze1997@gmail.com>
2024-07-24 15:10:35 +0400

    chore: Update sitemap URLs with latest lastmod dates

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,26 +3,26 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: "https://www.next-hub.pro",
-  generateRobotsTxt: true, // Generate robots.txt file
-  sitemapSize: 5000, // Max number of URLs per sitemap file
-  changefreq: "daily", // Change frequency of the URLs
-  priority: 0.7, // Priority of the URLs
-  exclude: ["/admin/*", "/login"], // Paths to exclude from the sitemap
+  generateRobotsTxt: true,
+  sitemapSize: 5000,
+  changefreq: "daily",
+  priority: 0.7,
+  exclude: ["/admin/*", "/login"],
   alternateRefs: [
     {
       href: "https://es.next-hub.pro",
       hreflang: "es",
     },
-  ], // List of alternate URLs for different languages
+  ],
   transform: async (config, path) => {
     return {
-      loc: path, // Use the default location
+      loc: path,
       changefreq: config.changefreq,
       priority: config.priority,
       lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
       alternateRefs: config.alternateRefs ?? [],
     };
-  }, // Function to modify sitemap fields
+  },
   additionalPaths: async (config) => [
     await config.transform(config, "/work"),
     await config.transform(config, "/about"),
@@ -34,7 +34,7 @@ module.exports = {
     await config.transform(config, "/services/seo"),
     await config.transform(config, "/services/soc_media"),
     await config.transform(config, "/services/tech_support"),
-  ], // Additional paths to include in the sitemap
+  ],
   robotsTxtOptions: {
     policies: [
       {
@@ -49,11 +49,11 @@ module.exports = {
         userAgent: "*",
         disallow: "/admin",
       },
-    ], // Policies for robots.txt
+    ],
     additionalSitemaps: [
       "https://www.next-hub.pro/my-custom-sitemap-1.xml",
       "https://www.next-hub.pro/my-custom-sitemap-2.xml",
-    ], // Additional sitemaps
+    ],
   },
-  autoLastmod: true, // Automatically set last modified date
+  autoLastmod: true,
 };
