43e871f78a86784105acc29e7031d293c9fe0945
giorgibliadze <bliadze1997@gmail.com>
2024-07-24 17:46:59 +0400

    chore: Update Layout.js with Google Tag Manager script and meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,7 @@ import { DefaultSeo } from "next-seo";
 import Script from "next/script";
 import Head from "next/head";
 import { GoogleTagManager } from "@next/third-parties/google";
+import { NextSeo } from "next-seo";
 // Font settings
 const sora = Sora({
   subsets: ["latin"],
@@ -18,7 +19,18 @@ const sora = Sora({
 const Layout = ({ children }) => {
   return (
     <>
+    <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-MCWYDWRTCY"
+      ></Script>
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-MCWYDWRTCY');`}
+      </Script>
       <Head>
+      <meta name="robots" content="index, follow" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -30,33 +42,66 @@ const Layout = ({ children }) => {
             }),
           }}
         />
-        <link rel="icon" href="/favicon.jpg" sizes="any" type="image/jpg" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
+        <meta
+          name="google-site-verification"
+          content="AV_Rqc6RXH1HpfaYzYb7la24kvaGW8sqHpeTaW2NU50"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-        <Head>
-          <meta name="robots" content="index, follow" />
-          <meta name="changefreq" content="always" />
-        </Head>
       </Head>
+      <GoogleTagManager gtmId="GTM-KHQ9N3M5" />
+      <NextSeo
+        title="Next-Hub Solutions | Home"
+        description="მიაღწიეთ თქვენს მიზნებს, გამოიყენეთ სრული პოტენციალი და გაზარდეთ ბრენდის ცნობადობა და სანდოობა next-hub-ის გამოცდილ გუნდთან ერთად."
+        keywords="Next-Hub, ციფრული რეალობა, იდეები, სურვილები, ოცნებები, ბრენდის ცნობადობა, სანდოობა, ციფრული ტრანსფორმაცია, ვებ აპლიკაციები, სოციალური მედია, SEO, ანალიტიკა"
+        openGraph={{
+          type: "website",
+          locale: "ka_GE",
+          url: "https://www.next-hub.pro",
+          site_name: "Next-Hub Solutions",
+          title: "Next-Hub Solutions | Home",
+          description:
+            "მიაღწიეთ თქვენს მიზნებს, გამოიყენეთ სრული პოტენციალი და გაზარდეთ ბრენდის ცნობადობა და სანდოობა next-hub-ის გამოცდილ გუნდთან ერთად.",
+          images: [
+            {
+              url: "https://www.next-hub.pro/nexthub.jpg",
+              width: 800,
+              height: 600,
+              alt: "Next-Hub Solutions",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@NextHub",
+          site: "@NextHub",
+          cardType: "summary_large_image",
+          title: "Next-Hub Solutions | Home",
+          description:
+            "მიაღწიეთ თქვენს მიზნებს, გამოიყენეთ სრული პოტენციალი და გაზარდეთ ბრენდის ცნობადობა და სანდოობა next-hub-ის გამოცდილ გუნდთან ერთად.",
+          image: "https://www.next-hub.pro/nexthub.jpg",
+        }}
+        additionalMetaTags={[
+          {
+            name: "keywords",
+            content:
+              "Next-Hub, ციფრული რეალობა, იდეები, სურვილები, ოცნებები, ბრენდის ცნობადობა, სანდოობა, ციფრული ტრანსფორმაცია, ვებ აპლიკაციები, სოციალური მედია, SEO, ანალიტიკა",
+          },
+          {
+            name: "author",
+            content: "Next-Hub Solutions",
+          },
+          {
+            name: "viewport",
+            content: "width=device-width, initial-scale=1.0",
+          },
+          {
+            name: "robots",
+            content: "index, follow",
+          },
+          {
+            name: "changefreq",
+            content: "always",
+          },
+        ]}
+      />
       <div
         className={`page bg-site text-white bg-cover bg-no-repeat ${sora.variable} font-sora relative overflow-y-auto`}
       >
