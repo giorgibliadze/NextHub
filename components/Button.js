6d800cc3c5fce7c063ce497a8b528a699af45d01
giorgibliadze <bliadze1997@gmail.com>
2024-07-29 23:54:39 +0400

    add id for events

--- components/Button.js
+++ components/Button.js
@@ -4,6 +4,7 @@ import React from "react";
 const Button = ({ onClick, children }) => {
   return (
     <button
+      id="purchase-button"
       className="relative w-[150px] lg:w-[200px] py-3 rounded-full bg-gradient-to-r from-purple-800 to-purple-500 text-white hover:from-purple-900 hover:to-purple-500 transition-colors duration-300 z-10"
       onClick={(e) => {
         e.stopPropagation();
