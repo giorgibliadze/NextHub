7bed9ad630cd375bb338a34008852bcd5652179c
giorgibliadze <bliadze1997@gmail.com>
2025-08-29 22:34:40 +0400

    width 500:500

--- pages/index.js
+++ pages/index.js
@@ -60,8 +60,8 @@ const Home = () => {
           images: [
             {
               url: "https://www.next-hub.pro/web.jpg",
-              width: 600,
-              height: 600,
+              width: 500,
+              height: 500,
               alt: "Next-Hub Solutions",
             },
           ],
