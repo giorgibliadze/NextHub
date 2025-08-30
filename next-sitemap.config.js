253c70a445fdfe1b452f98a04c103c833f39e2ea
giorgibliadze <bliadze1997@gmail.com>
2025-08-30 19:25:54 +0400

    url update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://www.next-hub.pro",
+  siteUrl: "https://next-hub.pro",
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: "daily",
